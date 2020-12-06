b770723cc3b1eba284ebbd81edce7ba1a057df50
skypedanny <skypedanny@naver.com>
2020-12-06 13:31:25 +0900

    [UPDATE] Setting

--- src/typing.d.ts
+++ src/typing.d.ts
@@ -1,4 +1,13 @@
+declare module ".png";
+declare module ".jpg";
+declare module ".jpeg";
+declare module ".gif";
+declare module ".bmp";
+declare module ".tiff";
+
 declare module "*.svg" {
-    const content: any;
-    export default content;
-  }
+  import React = require("react");
+  export const ReactComponent: React.SFC<React.SVGProps<SVGSVGElement>>;
+  const src: string;
+  export default src;
+}
