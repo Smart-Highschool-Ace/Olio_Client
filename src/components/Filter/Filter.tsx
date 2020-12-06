92858f193861633342dde3b0a2616ff02b8a9863
skypedanny <skypedanny@naver.com>
2020-12-06 13:31:00 +0900

    [UPDATE] Apply next/router

--- src/components/Filter/Filter.tsx
+++ src/components/Filter/Filter.tsx
@@ -1,27 +1,79 @@
-import React from 'react';
-import * as S from './Style';
-import { Setting } from 'Assets/Svg';
+import React, { useEffect, useState } from "react";
+import * as S from "./Style";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import Setting from "assets/svg/Setting.svg";
+import { setgroups } from "process";
+
+const filterList = [
+  {
+    router: "all",
+    title: "ALL",
+    //link: <Link href="/[home]" as="/home"></Link>,
+  },
+  {
+    router: "webs",
+    title: "WEB",
+  },
+  {
+    router: "androids",
+    title: "ANDROID",
+  },
+  {
+    router: "ios",
+    title: "IOS",
+  },
+  {
+    router: "ai",
+    title: "AI",
+  },
+  {
+    router: "databases",
+    title: "DATABASE",
+  },
+  {
+    router: "aduinos",
+    title: "ADUINO",
+  },
+];
+
+const useUser = () => ({ user: null, loading: false });
 
 const Filter: React.FC = () => {
-    return (
-        <S.Positioner>
-            <S.Wrapper>
-                <div className="filter">
-                    <Setting />
-                    <span>Filters</span>
-                </div>
-                <S.Navigation>
-                    <span>ALL</span>
-                    <span>WEB</span>
-                    <span>ANDROID</span>
-                    <span>IOS</span>
-                    <span>AI</span>
-                    <span>DATABASE</span>
-                    <span>ADUINO</span>
-                </S.Navigation>
-            </S.Wrapper>
-        </S.Positioner>
-    );
-  };
-  
+  const nextRouter = useRouter();
+  const { user, loading } = useUser();
+  const { id } = nextRouter.query;
+
+  useEffect(() => {
+    if (!(user || loading)) {
+      //nextRouter.push("/home/all");
+      console.log("page name : " + id);
+    }
+  }, [user, loading, id]);
+
+  const mappingMenuList = filterList.map((data, idx) => (
+    <Link href="/home/[id]" as={`/home/${data.router}`}>
+      <S.Navigation
+        key={idx}
+        //onClick={() => nextRouter.push(`/home/${data.router}`)}
+        style={{ textDecoration: "none" }}
+      >
+        {data.title}
+      </S.Navigation>
+    </Link>
+  ));
+
+  return (
+    <S.Positioner>
+      <S.FiltersButton>
+        <Setting />
+        <span>Filters</span>
+      </S.FiltersButton>
+      <S.Wrapper>
+        <S.NavWrapper>{mappingMenuList}</S.NavWrapper>
+      </S.Wrapper>
+    </S.Positioner>
+  );
+};
+
 export default Filter;
--- src/components/Filter/Style.ts
+++ src/components/Filter/Style.ts
@@ -1,64 +1,65 @@
-import styled from '@emotion/styled';
+import styled from "@emotion/styled";
 
 export const Positioner = styled.div`
-    width: 100%;
-    height: 108px;
-    display: flex;
-    align-items: center;
-    margin-bottom: 138px;
-`
+  width: 100%;
+  height: 108px;
+  display: flex;
+  align-items: center;
+  margin-bottom: 100px;
+`;
 
 export const Wrapper = styled.div`
-    width: 100%;
-    height: 100%;
-    display:flex;
-    align-items: center;
-    padding: 0 5%;
+  width: 100%;
+  margin-right: 3%;
+  display: flex;
+  justify-content: center;
+`;
 
-    .filter {
-        width: 95px;
-        height: 40px;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        border: 3px solid #82B897;
-        box-sizing: border-box;
-        border-radius: 5px;
-        
-        span {
-            font-family: Fira Sans;
-            font-style: normal;
-            font-weight: bold;
-            font-size: 16px;
-            line-height: 19px;
-        }
-        
-        color: #82B897;
-        cursor: pointer;
-    }
-`
+export const FiltersButton = styled.div`
+  width: 95px;
+  height: 40px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border: 3px solid #82b897;
+  box-sizing: border-box;
+  border-radius: 5px;
+  margin-left: 2%;
+
+  span {
+    font-family: Fira Sans;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 16px;
+    line-height: 19px;
+  }
+
+  color: #82b897;
+  cursor: pointer;
+`;
+
+export const NavWrapper = styled.div`
+  min-width: 1106px;
+  height: 30px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
 
 export const Navigation = styled.div`
-    width: 1106px;
-    height: 30px;
-    justify-content: space-between;
-    align-items: center;
-    display: flex;
-    margin-left: 10%;
-    span {
-        color: #ABD995;
-        font-family: Fira Sans;
-        font-style: normal;
-        font-weight: 600;
-        font-size: 20px;
-        line-height: 24px;
-        padding: 2px 22px;
-        border-radius: 5px;
+  color: #abd995;
+  font-family: Fira Sans;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 20px;
+  line-height: 24px;
+  padding: 2px 22px;
+  border-radius: 5px;
+  text-decoration: none;
 
-        cursor: pointer;
-        :hover {
-            color: #ffffff;
-            background-color: #ABD995;
-        }
-    }
-`
+  cursor: pointer;
+  :hover {
+    color: #ffffff;
+    background-color: #abd995;
+  }
+`;
